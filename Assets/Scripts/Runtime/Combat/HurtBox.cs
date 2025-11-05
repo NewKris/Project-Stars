ec9ae93d5f97fbf783fea835cdc61336497db084
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2025-11-05 09:38:26 +0100

    revert contact damage code

--- Assets/Scripts/Runtime/Combat/HurtBox.cs
+++ Assets/Scripts/Runtime/Combat/HurtBox.cs
@@ -8,11 +8,6 @@ namespace NewKris.Runtime.Combat {
         public Faction isFaction;
         public UnityEvent onHurt;
         public UnityEvent onDeath;
-
-        [Header("Contact Damage")] 
-        public bool canTakeContactDamage = true;
-        public int contactDamage;
-        public Faction canBumpFaction;
         
         private int _health;
 
@@ -24,26 +19,13 @@ namespace NewKris.Runtime.Combat {
                 onDeath.Invoke();
             }
         }
-
-        private void OnCollisionEnter(Collision other) {
-            if (other.gameObject.TryGetComponent(out HurtBox hurtBox) 
-                && CanHurtFaction(hurtBox.isFaction) 
-                && hurtBox.canTakeContactDamage
-            ) {
-                hurtBox.TakeDamage(contactDamage);
-            }
-        }
-
+        
         private void Reset() {
             gameObject.layer = LayerMask.NameToLayer("Hurt Box");
         }
 
         private void OnEnable() {
             _health = maxHealth;
-        }
-        
-        private bool CanHurtFaction(Faction faction) {
-            return (canBumpFaction & faction) != 0;
         }
     }
 }
