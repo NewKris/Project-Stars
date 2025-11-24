16e81e2b4e72d5b02d30f579e80b7351f2c38aaf
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2025-11-24 19:53:22 +0100

    allow character controller to push rigid bodies

---
+++ Assets/Scripts/Runtime/SpaceCombat/Mecha/CharacterControllerPush.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace Werehorse.Runtime.SpaceCombat.Mecha {
+    public class CharacterControllerPush : MonoBehaviour {
+        public float pushForce;
+        
+        private void OnControllerColliderHit(ControllerColliderHit hit) {
+            Rigidbody body = hit.collider.attachedRigidbody;
+
+            if (body == null || body.isKinematic) {
+                return;
+            }
+
+            body.AddForce(hit.moveDirection * pushForce, ForceMode.Impulse);
+        }
+    }
+}
